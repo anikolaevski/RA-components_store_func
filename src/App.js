0e36fdb1f1ed03cff7b9200b54a8230fcd0a3fb5
Alexey Nikolaevskiy <anikolaevski@gmail.com>
2021-02-14 20:43:23 +0300

    created

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+import './css/main.css';
+import ItemCard from './ItemCard.js';
+
+function App() {
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: 'GBP'
+  }
+
+  return ItemCard(item);
+}
+
+export default App;
